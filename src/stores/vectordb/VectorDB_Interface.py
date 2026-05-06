35b321d790542987987990f692ac2f1bf5c91c15
Mohamed Mansour <mm1757496@gmail.com>
2026-05-06 15:22:49 +0300

    Add VectorDB_Interface: define abstract methods for vector database operations

---
+++ src/stores/vectordb/VectorDB_Interface.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+class VectorDB_Interface(ABC):
+    @abstractmethod
+    def connect(self):
+            pass
+
+    @abstractmethod
+    def disconnect(self):
+            pass
+
+    @abstractmethod
+    def is_collection_excisted(self):
+            pass
+
+    @abstractmethod
+    def list_all_collections(self):
+            pass
+
+    @abstractmethod
+    def get_collection_info(self):
+            pass
+
+    @abstractmethod
+    def delete_collection(self):
+            pass
+    @abstractmethod
+    def create_collection(self, collection_name, dimension, do_reset=False):
+            pass
+    @abstractmethod
+    def insert_data(self, collection_name: str, text: str, vector: list, metadata=None, record_id=None):
+            pass
+    @abstractmethod
+    def insert_many_data(self, collection_name: str,
+                          texts: list, vectors: list,
+                            metadatas=None,
+                              record_ids=None,
+                              batch_size=50,):
+            pass
+    @abstractmethod
+    def search_by_vector(self, collection_name: str, vector: list, top_k: int = 5, include_metadata: bool = False):
+            pass
+    
