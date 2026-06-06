e7a01c2706fc2673c5af853c7b9ecd44df8a61a9
Mohamed Mansour <mm1757496@gmail.com>
2026-06-06 15:13:29 +0300

    Update __init__.py files to import models from mini_rag.schemes and create new __init__.py files for mini_rag and its schemes

--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,4 +1,9 @@
-from .project import Project
-from .data_chunk import DataChunk
-from .asset import Asset
-from .data_chunk import RetrievedDocument
+# from .project import Project
+# from .data_chunk import DataChunk
+# from .asset import Asset
+# from .data_chunk import RetrievedDocument
+
+
+from models.db_schemes.mini_rag.schemes import DataChunk, Asset, Project, RetrievedDocument
+
+
---
+++ src/models/db_schemes/mini_rag/schemes/__init__.py
@@ -0,0 +1,4 @@
+from .asset import Asset
+from .DataChunk import DataChunk, RetrievedDocument
+from .project import Project
+from .mini_rag_base import SqlAlchemyBase
